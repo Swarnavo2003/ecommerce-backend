f1d35bafc039ce6409f955fce3707775b9eb58ca
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-01 20:46:28 +0530

    feat: user entity added

---
+++ src/main/java/in/swarnavo/ecommerce_backend/model/User.java
@@ -0,0 +1,48 @@
+package in.swarnavo.ecommerce_backend.model;
+
+import in.swarnavo.ecommerce_backend.model.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 20)
+    private String LastName;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/in/swarnavo/ecommerce_backend/model/enums/UserRole.java
@@ -0,0 +1,7 @@
+package in.swarnavo.ecommerce_backend.model.enums;
+
+public enum UserRole {
+    USER,
+    SELLER,
+    ADMIN
+}
