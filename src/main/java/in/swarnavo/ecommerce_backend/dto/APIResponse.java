fab90c572764809990b75201b744d478eb0c65c8
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-01 22:37:27 +0530

    feat: classes for exception handling added

--- src/main/java/in/swarnavo/ecommerce_backend/dto/APIResponse.java
+++ src/main/java/in/swarnavo/ecommerce_backend/dto/APIResponse.java
@@ -1,4 +1,13 @@
 package in.swarnavo.ecommerce_backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class APIResponse {
+    private String message;
+    private boolean status;
 }
