31837c1f3b0bb79368504caedb0afc5e7e41d231
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-02 12:43:27 +0530

    feat: global api response added

---
+++ src/main/java/in/swarnavo/ecommerce_backend/advice/ApiResponse.java
@@ -0,0 +1,23 @@
+package in.swarnavo.ecommerce_backend.advice;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+    private LocalDateTime timestamp;
+
+    private T data;
+
+    public ApiResponse() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data) {
+        this();
+        this.data = data;
+    }
+}
