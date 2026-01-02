20c1441acfaff5dfb56244bf0fecd1be24bf7c0c
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-02 17:52:56 +0530

    feat: product entity added

---
+++ src/main/java/in/swarnavo/ecommerce_backend/model/Product.java
@@ -0,0 +1,53 @@
+package in.swarnavo.ecommerce_backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+@ToString
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain at least 2 characters")
+    private String productName;
+
+    private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain least 6 characters")
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
