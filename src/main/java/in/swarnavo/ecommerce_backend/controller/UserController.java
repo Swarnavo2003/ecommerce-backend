ab288e83630b1d67117ead15ed6b526b07fe42e2
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-07 18:23:21 +0530

    feat: swagger annotation added to user controller for better docs

--- src/main/java/in/swarnavo/ecommerce_backend/controller/UserController.java
+++ src/main/java/in/swarnavo/ecommerce_backend/controller/UserController.java
@@ -5,6 +5,8 @@ import in.swarnavo.ecommerce_backend.dto.LoginResponse;
 import in.swarnavo.ecommerce_backend.dto.RegisterDTO;
 import in.swarnavo.ecommerce_backend.dto.UserDTO;
 import in.swarnavo.ecommerce_backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,11 +23,15 @@ public class UserController {
 
     private final UserService userService;
 
+    @Tag(name = "Auth APIs", description = "APIs for managing authentication and user creation")
+    @Operation(summary = "Register User", description = "API to register users")
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@Valid @RequestBody RegisterDTO request) {
         return new ResponseEntity<>(userService.register(request), HttpStatus.CREATED);
     }
 
+    @Tag(name = "Auth APIs", description = "APIs for managing authentication and user creation")
+    @Operation(summary = "Login User", description = "API to login users")
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginDTO request) {
         LoginResponse loginResponse = userService.login(request);
